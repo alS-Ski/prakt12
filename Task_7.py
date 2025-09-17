5b474397515749bc0a4c6e2db9bf5a7ff8e2d9f6
alS-Ski <a.simonov1@g.nsu.ru>
2025-09-17 10:56:34 +0700

    Update Task_7.py

--- Task_7.py
+++ Task_7.py
@@ -2,9 +2,9 @@ st = str(input())
 if st[-1] == '.' or st[-1] == '?' or st[-1] == '!':
 	st = st[:-1]
 sl = st.split(' ')
-men = len(st)
+minin = len(st)
 for i in range(len(sl)):
-	if len(sl[i]) < men:
-		men = len(sl[i])
-print(men)
+	if len(sl[i]) < minin:
+		minin = len(sl[i])
+print(minin)
 
